cc175e586450d4a2546f68472129bba572d795ca
RawanAli1993 <rawan.08.ali@gmail.com>
2024-11-12 12:33:17 +0300

    Initial commit

---
+++ lab08_solutions/animals/AnimalCreator.java
@@ -0,0 +1,46 @@
+package ru.makhmutov.lab.task1.animals;
+
+import java.util.Arrays;
+
+public class AnimalCreator {
+
+    /**
+     * The entry point of the Animals program.
+     * It allows creating array of different animals
+     * and demonstrate inheritance and polymorphism
+     * principles
+     *
+     * @param args Array with parameters of the program
+     */
+    public static void main(String[] args) {
+        Animal[] animals = addAnimals();
+        System.out.println(Arrays.toString(animals));
+        for (Animal animal : animals){
+            animal.eat();
+            animal.sleep();}
+    }
+
+    /**
+     * This method allows adding animals of different
+     * type to the array
+     *
+     * @return the array of animals
+     */
+    private static Animal[] addAnimals() {
+        Animal[] animals = new Animal[4];
+
+        Animal animal = new Animal(50, 35.5F, "black");
+        animals[0] = animal;
+
+        Animal dog = new Dog(40, 28F, "brown");
+        animals[1] = dog;
+
+        Animal cat = new Cat(10, 5.5F, "orange");
+        animals[2] = cat;
+
+        Animal cow = new Cow(160.5F, 450, "white");
+        animals[3] = cow;
+
+        return animals;
+    }
+}
---
+++ lab08_solutions/shapes/Shape.java
@@ -0,0 +1,6 @@
+package ru.makhmutov.lab.task1.shapes;
+
+public abstract class Shape {
+    protected abstract double calculatePerimeter();
+    protected abstract double calculateArea();
+}
